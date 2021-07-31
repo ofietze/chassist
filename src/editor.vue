8df02c2fe1f81a75a6281c32b82b80c2a403b4e1
ofietze <f.oliver@live.de>
2021-07-31 11:26:23 +0200

    Add basic files

---
+++ src/editor.vue
@@ -0,0 +1,16 @@
+<script>
+import { chessboard }  from 'vue-chessboard'
+export default {
+  name: 'editor',
+  extends: chessboard,
+  mounted() {
+    this.board.set({
+      movable: {
+        color: 'both',
+        free: true,
+        events: { after: undefined }
+      }
+    })
+  }
+}
+</script>
